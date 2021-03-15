dda0c925fcd6c74c822995ea90bfabe44f837151
dryvenn <dryvenn@gmail.com>
2021-03-15 18:00:05 +0000

    fish: fix kak invocation

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -2,8 +2,8 @@ function e -d "Start a kakoune server or client"
     set cur_dir (basename $PWD | tr -d '.' | tr '_' '-')
     set git_dir (basename (git rev-parse --show-toplevel 2> /dev/null; or pwd) | tr -d '.' | tr '_' '-')
     kak -clear; kak -clear
-    if jobs %kak &> /dev/null
-        fg %kak
+    if jobs | grep -q kak
+        fg %(jobs | grep kak | grep -Eo '^[[:digit:]]+') > /dev/null
     else if kak -l | grep -q "^$cur_dir\$"
         kak -c $cur_dir $argv
     else if kak -l | grep -q "^$git_dir\$"
