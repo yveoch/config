3ee02d877ce025a79524e619414bbb36248bb1ab
dryvenn <dryvenn@gmail.com>
2022-11-24 16:38:02 +0000

    fish: remove outdated keybindings

--- fish/conf.d/fzf_binds.fish
+++
@@ -1,5 +0,0 @@
-# see https://github.com/PatrickF1/fzf.fish#custom-key-bindings
-set --universal fzf_fish_custom_keybindings
-bind \ct __fzf_search_current_dir
-bind \cr __fzf_search_history
-bind \ch __fzf_search_git_log
